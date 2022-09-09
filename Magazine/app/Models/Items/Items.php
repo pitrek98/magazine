b271fd79cc8bca2ff75c4afc0fe264d7ead06ce1
Pszymiec <piotrekszymiec@gmail.com>
2022-09-09 19:31:37 +0200

    category Items

---
+++ Magazine/app/Models/Items/Items.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Items;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Items extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ["name", "price", "category_id"];
+
+    public function category()
+    {
+        $this->belongsTo(Category::class);
+    }
+}
