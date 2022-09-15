d729536255b153a156f8c5e2f55cb1bb76dbda09
Pszymiec <piotrekszymiec@gmail.com>
2022-09-15 22:03:08 +0200

    Added Controllers and Services for Items ((they dont work))

---
+++ Magazine/app/Http/Controllers/Items/ItemsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Items;
+
+use App\Http\Controllers\Controller;
+use App\Services\Items\ItemService;
+use App\Services\Items\ItemsService;
+use Illuminate\Http\Request;
+
+
+class ItemsController extends Controller
+{
+    public ItemsService $itemsService;
+
+    public function __construct(ItemsService $itemsService)
+    {
+        $this->itemsService = $itemsService;
+    }
+
+    public function list()
+    {
+        $items = $this->itemsService->list();
+        return response()->json(['items' => $items]);
+    }
+}
